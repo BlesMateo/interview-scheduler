019bf75a4be5b5be1e684978344feda078f30438
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-07 17:44:52 -0500

    Adding updates to files for day 2

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+
 import "index.scss";
 
 import Button from "components/Button";
