ad8a504fcce127eaea4baad47470fe69be9f6792
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-08 18:52:39 -0500

    Replaced hard coded values with props and added event handler to the list, classes and conditional rendering of the interviewers name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,23 @@
-import React from "react";
-import "components/InterviewerListItem.scss"
+import React from 'react';
 
- export default function InterviewerListItem(props) {
-   return (
-     <li className="interviewers__item">
-   <img
-     className="interviewers__item-image"
-     src="https://i.imgur.com/LpaY82x.png"
-     alt="Sylvia Palmer"
-   />
-   Sylvia Palmer
- </li>
-   );
- }
+import classNames from 'classnames';
+
+import "components/InterviewerListItem.scss";
+
+
+export default function InterviewListItem (props) {
+  let interviewerClass = classNames("interviewers__item",{
+    "interviewers__item--selected": props.selected
+  })
+
+  return (
+    <li onClick={props.setInterviewer} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
