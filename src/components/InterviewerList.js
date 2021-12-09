040fcafcf0263ac1e2d04b85f992580f7fb22e50
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-08 19:39:23 -0500

    Fixed the file to include return statement inside the function

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,11 +14,11 @@ export default function InterviewerList(props) {
       />
     );
   });
-}
 
 return (
   <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
     <ul className="interviewers__list">{interviewersArr}</ul>
   </section>
-)
+  )
+}
