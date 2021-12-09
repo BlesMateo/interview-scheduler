5e937b81677fcaae23e5beb6d9d8ecf4fbad8efb
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-09 15:11:06 -0500

    Added status component to index.js file

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -163,3 +164,5 @@ storiesOf("DayListItem", module)
       onCancel={action("onCancel")}
     />
   ))
+  .add("Status", () => <Status message="Deleting" />)
+
