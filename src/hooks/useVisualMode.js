ef4351211b5525f78416818901c518bc567e9ccb
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-12 16:13:42 -0500

    Created a transition function that updates the mode state with the new value

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,15 @@
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+
+/* Function that takes in a new mode and updates the mode state with the new value */
+const transition = function(newMode, replace = false) {
+  if (!replace) {
+    setHistory((prev) => [...prev, newMode]);
+  }
+  setMode(newMode);
+}
+
+
+return { mode };
 }
