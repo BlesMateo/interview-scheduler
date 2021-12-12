0c8373754ed852bc8a63ef376c36177ddd2dd0ff
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-12 16:34:13 -0500

    Modified the value of the third constant as a string

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -4,7 +4,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 const SECOND = "SECOND";
-const THIRD = THIRD;
+const THIRD = "THIRD";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -31,6 +31,13 @@ test("useVisualMode should return to previous mode", () => {
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
