da5e968f8f80c5217dc5a2cddaa77dc5582fe382
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-12 20:07:29 -0500

    Edited the test statements for getInterviewersForDay to match test results and added interviewer data inside of days

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,16 +1,18 @@
-import { getAppointmentsForDay, getInterview} from "helpers/selectors";
+import { getAppointmentsForDay, getInterviewersForDay, getInterview} from "helpers/selectors";
 
 const state = {
   days: [
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers: [1, 2, 3]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers: [4, 5]
     }
   ],
 
@@ -83,8 +85,8 @@ test("getInterviewersForDay returns an array with a length matching the number o
 
 test("getInterviewersForDay returns an array containing the correct interviewers objects", () => {
   const [first, second] = getInterviewersForDay(state, "Tuesday");
-  expect(first).toEqual(state.appointments["4"]);
-  expect(second).toEqual(state.appointments["5"]);
+  expect(first).toEqual(state.interviewers["4"]);
+  expect(second).toEqual(state.interviewers["5"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
