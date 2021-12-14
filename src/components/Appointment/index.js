0048b4a09389349ce95673e601ae6f3cca3850be
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-14 17:42:01 -0500

    Added props.interviewers inside of Form interviewers previously containing empty array

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,10 +5,16 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
+
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -16,6 +22,7 @@ export default function Appointment(props) {
   );
 
   function save(name, interviewer) {
+    transition(SAVING)
     const interview = {
       student: name,
       interviewer
@@ -24,6 +31,15 @@ export default function Appointment(props) {
       transition(SHOW);
     })
   }
+
+  function deleteAppointment(name, interviewer) {
+    console.log("++++++++++")
+    transition(DELETING)
+    props.cancelInterview(props.id)
+    .then(() => {
+      transition (EMPTY)
+    })
+  };
 
   return (
     <article className="appointment">
@@ -35,7 +51,14 @@ export default function Appointment(props) {
        interviewer={props.interview.interviewer}
        />
        )}
-       {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
+       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()} onSave={save} />}
+       {mode === SAVING && <Status
+       message={"Saving"}/>}
+       {mode === CONFIRM && <Confirm
+       onCancel={() => back()}
+       message={"Are you would like to delete?"}
+       onConfirm={deleteAppointment}/>}
+
    </article>
-  )
+ )
 }
