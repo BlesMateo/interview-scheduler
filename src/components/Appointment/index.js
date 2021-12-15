2a814231a44efe743591534aadddb1966bed111e
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-14 22:34:53 -0500

    Added semicolon to the end of the imported styles.scss

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
@@ -25,63 +25,82 @@ export default function Appointment(props) {
   );
 
   function save(name, interviewer) {
-    transition(SAVING)
+    transition(SAVING);
     const interview = {
       student: name,
-      interviewer
+      interviewer,
     };
-    props.bookInterview(props.id, interview).then(() => {transition(SHOW) })
-    .catch(() => transition(ERROR_SAVE, true))
+    props
+      .bookInterview(props.id, interview)
+      .then(() => {
+        transition(SHOW);
+      })
+      .catch(() => transition(ERROR_SAVE, true));
   }
 
   function deleteAppointment() {
-    transition(DELETING, true)
-    props.cancelInterview(props.id)
-    .then(() => {
-      transition(EMPTY)
-    })
-    .catch(() => transition(ERROR_DELETE, true))
-  };
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch(() => transition(ERROR_DELETE, true));
+  }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-       <Show
-       student={props.interview.student}
-       interviewer={props.interview.interviewer}
-       onDelete={() => transition(CONFIRM)}
-       onEdit={() => transition(EDIT)}
-       />
-       )}
-       {mode === CREATE && <Form
-       interviewers={props.interviewers}
-       onCancel={() => back()} onSave={save} />}
-
-       {mode === SAVING && <Status
-       message={"Saving"}/>}
-
-       {mode === CONFIRM && <Confirm
-       onCancel={() => back()}
-       message={"Are you would like to delete?"}
-       onConfirm={deleteAppointment}/>}
-
-       {mode === EDIT && ( <Form
-       interviewer={props.interview.interviewer.id}
-       interviewers={props.interviewers}
-       student={props.interview.student}
-       onCancel={() => back()}
-       onSave={save} />
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onCancel={() => back()}
+          onSave={save}
+        />
       )}
 
-      {mode === ERROR_SAVE && <Error
-      message={"Could not create appointment"}
-      onClose={() => back()} />}
+      {mode === SAVING && <Status message={"Saving"} />}
 
-      {mode === ERROR_DELETE && <Error
-      message={"Could not cancel appointment"}
-      onClose={() => back()} />}
-   </article>
- )
+      {mode === CONFIRM && (
+        <Confirm
+          onCancel={() => back()}
+          message={"Are you would like to delete?"}
+          onConfirm={deleteAppointment}
+        />
+      )}
+
+      {mode === EDIT && (
+        <Form
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          student={props.interview.student}
+          onCancel={() => back()}
+          onSave={save}
+        />
+      )}
+
+      {mode === ERROR_SAVE && (
+        <Error
+          message={"Could not create appointment"}
+          onClose={() => back()}
+        />
+      )}
+
+      {mode === ERROR_DELETE && (
+        <Error
+          message={"Could not cancel appointment"}
+          onClose={() => back()}
+        />
+      )}
+    </article>
+  );
 }
