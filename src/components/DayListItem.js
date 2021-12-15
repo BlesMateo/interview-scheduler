406fe5093a579c09cdbf2d8926f501044a3cb8c6
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-15 18:07:52 -0500

    Added data-testid attribute that has the day value

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,21 +5,25 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--full": props.spots === 0,
   });
 
   const formatSpots = () => {
-    if(props.spots === 0) {
-      return "no spots remaining"
+    if (props.spots === 0) {
+      return "no spots remaining";
     }
-    if(props.spots === 1) {
-      return `${props.spots} spot remaining`
+    if (props.spots === 1) {
+      return `${props.spots} spot remaining`;
     }
-    return `${props.spots} spots remaining`
-  }
+    return `${props.spots} spots remaining`;
+  };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
