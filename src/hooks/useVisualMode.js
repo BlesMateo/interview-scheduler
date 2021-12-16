fe16cf8852be591483471c5bb4ed6f44598cef9c
BlesMateo <blesildaangelicamateo@gmail.com>
2021-12-15 22:29:34 -0500

    Reformatted code for appropriate indentation

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,24 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  /* Function that takes in a new mode and updates the mode state with the new value */
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
+    setMode(newMode);
+  };
 
-/* Function that takes in a new mode and updates the mode state with the new value */
-const transition = (newMode, replace = false) => {
-  if (!replace) {
-    setHistory((prev) => [...prev, newMode]);
-  }
-  setMode(newMode);
-};
+  /* Function that sets the mode to the previous item in the history array*/
+  const back = () => {
+    if (history.length > 1) history.pop();
+    const formerMode = history[history.length - 1];
+    return setMode(formerMode);
+  };
 
-/* Function that sets the mode to the previous item in the history array*/
-const back = () => {
-  if (history.length > 1) history.pop();
-  const formerMode = history[history.length - 1];
-  return setMode(formerMode)
-};
-
-return { mode, transition, back };
-};
+  return { mode, transition, back };
+}
